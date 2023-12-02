f9c588b218ee9269888933ca6b508baa91934ca3
arpit <arpit2003y@gmail.com>
2023-12-02 19:50:01 +0530

    modified qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -59,33 +59,21 @@ def open_github():
 
 
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
-    # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(),
         desc="Move window focus to other window"),
-
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
     Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
         desc="Move window to the left"),
     Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
         desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
     Key(
         [mod, "shift"],
         "Return",
@@ -93,69 +81,29 @@ keys = [
         desc="Toggle between split and unsplit sides of stack",
     ),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod], "x", lazy.shutdown(), desc="Shutdown Qtile"),
-    # Key([mod], "d", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     Key([mod], "d", lazy.spawn("sh -c 'mpv --no-video ~/.config/startupSounds/menu-01.mp3 & dmenu_run'"),
         desc="play specific PMV file and launch dmenu"),
-
-
-    # Adjust brightness up
     Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
-
-    # Adjust brightness down
     Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
-    # reboot
-    Key([mod], "z", lazy.spawn("reboot"),
-        desc="rebooting the system instantly"),
-    # running the monitor screen layout scripts
+    Key([mod], "z", lazy.spawn("reboot"),desc="rebooting the system instantly"),
     Key([mod, "shift"], "s", lazy.spawn(
         "bash /home/darkxx/.screenlayout/monitor.sh")),
     Key([mod, "shift"], "s", lazy.spawn("shutdown")),
-    # grow window
     Key([mod], "h", lazy.layout.grow()),
-    # shrink window
     Key([mod], "l", lazy.layout.shrink()),
-    # Floating windows
     Key([mod], "f", lazy.window.toggle_floating(
         border_focus="#fff"), desc="Toggle floating"),
-    # Increase volume
     Key([], "XF86AudioRaiseVolume", lazy.spawn(
         "pactl set-sink-volume @DEFAULT_SINK@ +5%")),
-
-    # Decrease volume
     Key([], "XF86AudioLowerVolume", lazy.spawn(
         "pactl set-sink-volume @DEFAULT_SINK@ -5%")),
-
-    # Mute/Unmute
     Key([], "XF86AudioMute", lazy.spawn(
         "pactl set-sink-mute @DEFAULT_SINK@ toggle")),
-    
-
-
 ]
-
-
-keys.extend([
-
-    # Toggle Scratchpad
-])
-
-# calculations for the screen size
-screen_width = 1366
-screen_height = 768
-width = 0.6
-height = 0.4
-x = (1 - width) / 2
-y = (1 - height) / 2
-
-scratchpad = ScratchPad("scratchpad", [
-    DropDown("kitty", "kitty", opacity=0.9, width=width, height=height, x=x, y=y, on_focus_lost_hide=True),
-])
-
 
 
 groups = []
@@ -174,8 +122,6 @@ for i in range(len(group_names)):
             label=group_labels[i],
         ))
 
-# groups.append(scratchpad)
-
 for i in groups:
     keys.extend([
         Key([mod], i.name, lazy.group[i.name].toscreen()),
@@ -183,11 +129,8 @@ for i in groups:
         Key([mod, "shift"], "Tab", lazy.screen.prev_group()),
         Key(["mod1"], "Tab", lazy.screen.next_group()),
         Key(["mod1", "shift"], "Tab", lazy.screen.prev_group()),
-
-        Key([mod, "shift"], i.name, lazy.window.togroup(
-            i.name), lazy.group[i.name].toscreen()),
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name), lazy.group[i.name].toscreen()),
         Key([mod], "space", lazy.group["scratchpad"].dropdown_toggle("kitty")),
-            # Key([], "Escape", lazy.group.set_focus(lazy.layout.history.get())),
     ])
 
 
@@ -217,7 +160,7 @@ widget_defaults = dict(
 )
 extension_defaults = widget_defaults.copy()
 
-# Create a TextBox widget as a separator
+# -- widget list --
 separator_widget = widget.TextBox(
     text="|",
     fontsize=14,
@@ -236,13 +179,6 @@ icon_ = widget.TextBox(
 )
 sep3 = widget.TextBox(
     text="]",
-    fontsize=14,
-    padding=5,
-    foreground="#ffc0cb",
-    background="#0f1014"
-)
-sep2 = widget.TextBox(
-    text="󰋙=",
     fontsize=14,
     padding=5,
     foreground="#ffc0cb",
@@ -321,8 +257,6 @@ screens = [
                     scale=0.7
                 ),
                 separator_widget,
-
-                sep2,
                 widget.WindowName(foreground="#9cd3f7",
                                   max_chars=15,
 
